896d1fd59174f01849fd44f468c8cedebd6f92a1
snwox <seonu64@gmail.com>
2021-12-19 21:47:02 +0900

    initial gh-pages commit

---
+++ st4rt/src/App.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { Router, Route, Link } from "svelte-navigator";
+    import Home from "./routes/Home.svelte";
+    import About from "./routes/About.svelte";
+    import Members from "./routes/Members.svelte";
+    $: visible = false;
+    function toggle_btns() {
+        visible = visible ? false : true;
+    }
+</script>
+
+<Router>
+    <div>
+        <h1>ST4RT</h1>
+        <img src="images/st4rt.png" alt="Image Error" on:click={toggle_btns} />
+    </div>
+    {#if visible}
+        <div>
+            <Link to="/">Base</Link>
+            <Link to="home">Home</Link>
+            <Link to="about">About</Link>
+            <Link to="members">members</Link>
+        </div>
+    {/if}
+
+    <Route path="home" component={Home} />
+    <Route path="about" component={About} />
+    <Route path="members" component={Members} />
+</Router>
+
+<style>
+    h1 {
+        color: rgb(233, 221, 221);
+    }
+
+    img {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+    }
+</style>
